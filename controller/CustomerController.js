cad869ee93689843dd765b916cceb3f877891a1f
vyeung <vl.yeung@yahoo.com>
2019-06-07 15:15:18 -0400

    add cars routes to CustomerController

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -4,7 +4,34 @@ var carsDao = require("../dao/CarsDao");
 
 
 //get all cars of a type
+routes.get("/cars/:carType", (req, res) => {
+  var carType = req.params.carType;
 
+  carsDao.getAllCarsOfType(carType, (err, result) => {
+    if(err) {
+      res.status(400).json({"error": err});
+    }
+    else {
+      res.status(200).send(result);
+    }
+  });
+});
+
+
+//get one car of a type
+routes.get("/cars/:carType/:carId", (req, res) => {
+  var carType = req.params.carType;
+  var carId = req.params.carId;
+
+  carsDao.getOneCarofType(carType, carId, (err, result) => {
+    if(err) {
+      res.status(400).json({"error": err});
+    }
+    else {
+      res.status(200).send(result);
+    }
+  });
+});
 
 
 //get all orders for specific user
@@ -21,10 +48,11 @@ routes.get("/orders/user/:id", (req, res) => {
   });
 });
 
+
 //add an order
 routes.post('/orders', (req, res) => {
   var order = req.body;
-  
+
   ordersDao.addOrder(order, (err, result) => {
     if(err) {
       res.status(400).json({"status": 400, "error": err});
