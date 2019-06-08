4b679df2ad4946102c7bf2f4396d2862c14f2378
vyeung <vl.yeung@yahoo.com>
2019-06-08 01:28:56 -0400

    add user routes to CustomerController

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -1,6 +1,7 @@
 var routes = require("express").Router();
 var ordersDao = require("../dao/OrdersDao");
 var carsDao = require("../dao/CarsDao");
+var userDao = require("../dao/UserDao");
 
 
 //get all cars of a type
@@ -16,7 +17,6 @@ routes.get("/cars/:carType", (req, res) => {
     }
   });
 });
-
 
 //get one car of a type
 routes.get("/cars/:carType/:carId", (req, res) => {
@@ -48,9 +48,8 @@ routes.get("/orders/user/:id", (req, res) => {
   });
 });
 
-
 //add an order
-routes.post('/orders', (req, res) => {
+routes.post("/orders", (req, res) => {
   var order = req.body;
 
   ordersDao.addOrder(order, (err, result) => {
@@ -59,6 +58,64 @@ routes.post('/orders', (req, res) => {
     }
     else { 
       res.status(201).send();
+    }
+  });
+});
+
+
+//get a user
+routes.get("/user/:id", (req, res) => {
+  var userId = req.params.id;
+
+  userDao.getUser(userId, (err, result) => {
+    if(err) {
+      res.status(400).json({"status": 400, "error": err});
+    }
+    else { 
+      res.status(200).send(result);
+    }
+  });
+});
+
+//add a user
+routes.post("/user", (req, res) => {
+  var user = req.body;
+
+  userDao.addUser(user, (err, result) => {
+    if(err) {
+      res.status(400).json({"status": 400, "error": err});
+    }
+    else { 
+      res.status(201).send();
+    }
+  });
+});
+
+//update a user
+routes.put("/user/:id", (req, res) => {
+  var user = req.body;
+  var userId = req.params.id;
+
+  userDao.updateUser(user, userId, (err, result) => {
+    if(err) {
+      res.status(400).json({"status": 400, "error": err});
+    }
+    else { 
+      res.status(204).send();
+    }
+  });
+});
+
+//delete a user
+routes.delete("/user/:id", (req, res) => {
+  var userId = req.params.id;
+
+  userDao.deleteUser(userId, (err, result) => {
+    if(err) {
+      res.status(400).json({"status": 400, "error": err});
+    }
+    else { 
+      res.status(204).send();
     }
   });
 });
