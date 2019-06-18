ba9d910d2af2914c38c53a9ae3180e1edc5c9fc8
vyeung <vl.yeung@yahoo.com>
2019-06-18 14:55:13 -0400

    reworked OrdersDao

--- dao/OrdersDao.js
+++ dao/OrdersDao.js
@@ -6,46 +6,67 @@ exports.getAllUserOrders = (userId, cb) => {
     if(err)
       return cb(err, null);
     if(result.length === 0)
-      return cb("userId does not exist", null);
+      return cb(`userId=${userId} does not exist`, null);
 
     var counter = 0;
     result.forEach((obj) => {
       db.query("SELECT * FROM tbl_user WHERE userId=?", obj.userId, (err, userObj) => {
         //replace userId with its object version
         result[counter].userId = userObj[0];  
-      });
 
-      db.query("SELECT * FROM tbl_car WHERE carId=?", obj.carId1, (err, car1Obj) => {
-        //replace carId1 with its object version
-        result[counter].carId1 = car1Obj[0];  
-      });
-
-      db.query("SELECT * FROM tbl_car WHERE carId=?", obj.carId2, (err, car2Obj) => {
-        if(obj.carId2 === null) {
-          result[counter].carId2 = null;
-        }
-        else {
-          result[counter].carId2 = car2Obj[0];
-        }
-  
         counter++;
         if(counter === result.length) {
           cb(null, result);  //sends final result when forEach is done
-        }        
+        } 
       });
-    });
+    });    
   });
 };
 
-exports.addOrder = (order, cb) => { 
-  //a connection is implicitly established by invoking query()
+exports.getAllItemsFromOrder = (orderId, cb) => {
+  db.query("SELECT orderId, carId FROM tbl_orderitem WHERE orderId=?", orderId, (err, result) => {
+    if(err)
+      return cb(err, null);
+    if(result.length === 0)
+      return cb(`orderId=${orderId} does not exist`, null);
+
+    var counter = 0;
+    result.forEach((obj) => {
+      db.query("SELECT * FROM tbl_car WHERE carId=?", obj.carId, (err, carObj) => {
+        //replace carId with its object version
+        result[counter].carId = carObj[0];  
+
+        counter++;
+        if(counter === result.length) {
+          cb(null, result);
+        } 
+      });
+    });    
+  });
+};
+
+exports.addOrder = (order, cb) => {
+  var boughtCarsArray = order.boughtCars;
+
   db.query(
-    `INSERT INTO tbl_order (userId, carId1, carId2, totalPrice, purchaseDate) VALUES (?,?,?,?,?)`,
-    [order.userId, order.carId1, order.carId2, order.totalPrice, order.purchaseDate],
+    `INSERT INTO tbl_order (userId, totalPrice, purchaseDate) VALUES (?,?,?)`,
+    [order.userId, order.totalPrice, order.purchaseDate],
     (err, result) => {
       if(err) 
         cb(err, null);
+
+      //get orderId of last row in tbl_order
+      db.query("SELECT orderId FROM tbl_order ORDER BY orderId DESC LIMIT 1", (err, lastRow) => { 
         
-      cb(null, result);
-    });
+        var counter = 0;
+        boughtCarsArray.forEach((ele) => {
+          //insert orderitems using just created orderId
+          db.query("INSERT INTO tbl_orderitem (orderId, carId) VALUES (?,?)", [lastRow[0].orderId, ele], (err, result) => {
+            counter++;
+            if(counter === boughtCarsArray.length)
+              cb();
+          });
+        });
+      });
+  });
 }
