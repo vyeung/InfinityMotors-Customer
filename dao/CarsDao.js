5c3b9d838c92044a59c6e4d3eeb917ca997d1cfb
vyeung <vl.yeung@yahoo.com>
2019-06-25 14:23:54 -0400

    rework CarsDao methods to use JOINs

--- dao/CarsDao.js
+++ dao/CarsDao.js
@@ -2,37 +2,66 @@ var db = require("../utils/db");
 
 
 exports.getAllCarsOfType = (carType, cb) => {
-  db.query("SELECT * FROM tbl_car WHERE type=?", carType, (err, result) => {
+  var sql = `SELECT * FROM tbl_car 
+             JOIN tbl_carspecs ON tbl_carspecs.carspecId = tbl_car.carspecId 
+             WHERE tbl_car.type = ?`;
+
+  db.query(sql, carType, (err, result) => {
     if(err)
       cb(err, null);
     if(result.length === 0)
       return cb("invalid car type", null);
-    
-    var counter = 0;
-    result.forEach((obj) => {
-      db.query("SELECT * FROM tbl_carspecs WHERE carspecId=?", obj.carspecId, (err, carSpecObj) => {
-        //replace id with carspec object
-        result[counter].carspecId = carSpecObj[0];
-        
-        counter++;
-        if(counter === result.length) {
-          cb(null, result);  //sends final result when forEach is done
-        }
-      }); 
-    });
+
+    reformatJoinedCarSpecsResult(result, cb);
   });
 }
 
 exports.getOneCarofType = (carType, carId, cb) => {
-  db.query("SELECT * FROM tbl_car WHERE carId=? && type=?", [carId, carType], (err, result) => {
+  var sql = `SELECT * FROM tbl_car 
+             JOIN tbl_carspecs ON tbl_carspecs.carspecId = tbl_car.carspecId 
+             WHERE tbl_car.type=? && tbl_car.carId=?`;
+
+  db.query(sql, [carType, carId], (err, result) => {
     if(err)
       cb(err, null);
     if(result.length === 0)
       return cb(`${carType} with id=${carId} not found`, null);
 
-    db.query("SELECT * FROM tbl_carspecs WHERE carspecId=?", result[0].carspecId, (err, carSpecObj) => {
-      result[0].carspecId = carSpecObj[0];
-      cb(null, result);  //sends final result when done
-    }); 
+    reformatJoinedCarSpecsResult(result, cb);
   });
+}
+
+reformatJoinedCarSpecsResult = (result, cb) => {
+  var counter = 0;
+  var carSpec = {};
+  result.forEach((obj) => {
+    //manually create the carSpec object
+    carSpec.carspecId = obj.carspecId;
+    carSpec.engine = obj.engine;
+    carSpec.transmission = obj.transmission;
+    carSpec.color = obj.color;
+    carSpec.horsepower = obj.horsepower;
+    carSpec.weightLBS = obj.weightLBS;
+    carSpec.heightIN = obj.heightIN;
+    carSpec.lengthIN = obj.lengthIN;
+    carSpec.widthIN = obj.widthIN;
+
+    //add carSpec object to result
+    result[counter].carspecId = carSpec;
+
+    //remove misplaced values
+    delete result[counter].engine;
+    delete result[counter].transmission;
+    delete result[counter].color;
+    delete result[counter].horsepower;
+    delete result[counter].weightLBS;
+    delete result[counter].heightIN;
+    delete result[counter].lengthIN;
+    delete result[counter].widthIN;
+
+    counter++;
+    carSpec = {};  //reset
+  });
+
+  cb(null, result);  //done
 }
